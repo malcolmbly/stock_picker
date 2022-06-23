6aa3471f33094ad9bf2e60a136f82422f04d56ac
malcolmbly <malcolmhbailey@gmail.com>
2022-06-22 18:14:15 -0700

    initial commit

---
+++ stock_picker.rb
@@ -0,0 +1,6 @@
+def stockPicker(prices)
+    # take in an array of prices
+    # find the lowest day from the start until the second to last index
+    # then find the highest day after it
+    
+end
